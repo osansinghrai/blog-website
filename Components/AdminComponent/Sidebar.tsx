cd0734f73172deacb03e69a73c007a0fab283f74
Osan Rai <osanbantawarai@gmail.com>
2025-04-05 18:48:13 +0545

    feat: add blog submission form and update sidebar styling
    - Added a form in the admin addProduct page to submit blog details including title, description, category, author, and thumbnail image.
    - Updated sidebar links with improved styling and hover effects.
    - Added new dependencies `@radix-ui/react-slot` and `sonner` for enhanced UI functionality.

--- Components/AdminComponent/Sidebar.tsx
+++ Components/AdminComponent/Sidebar.tsx
@@ -11,15 +11,24 @@ const Sidebar = () => {
       </div>
       <div className="w-28 sm:w-80 h-[100vh] relative py-12 border border-black ">
         <div className="w-[50%] sm:w-[80%] absolute right-0 ">
-          <Link href='/admin/addProduct' className="flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-1px_1px_5px_#000000]  ">
+          <Link
+            href="/admin/addProduct"
+            className="mt-5 flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-1px_1px_2px_#000000] transition-all duration-200 active:scale-99 active:shadow-none "
+          >
             <Image src={assets.add_icon} alt="add" width={28} />
             <p>Add blogs</p>
           </Link>
-          <Link href="/admin/blogList" className="mt-5 flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-1px_1px_5px_#000000]  ">
+          <Link
+            href="/admin/blogList"
+            className="mt-5 flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-1px_1px_2px_#000000] transition-all duration-200 active:scale-99 active:shadow-none "
+          >
             <Image src={assets.blog_icon} alt="add" width={28} />
             <p>Blog lists</p>
           </Link>
-          <Link href="/admin/subscription" className="mt-5 flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-1px_1px_5px_#000000]  ">
+          <Link
+            href="/admin/subscription"
+            className="mt-5 flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-1px_1px_2px_#000000] transition-all duration-200 active:scale-99 active:shadow-none "
+          >
             <Image src={assets.email_icon} alt="add" width={28} />
             <p>Subscription</p>
           </Link>
--- app/admin/addProduct/page.tsx
+++ app/admin/addProduct/page.tsx
@@ -1,11 +1,43 @@
 "use client";
 
 import { assets } from "@/public/Assets/assets";
+import axios from "axios";
 import Image from "next/image";
 import React, { useState } from "react";
 
+
 const page = () => {
   const [image, setImage] = useState(false);
+
+  const [data, setData] = useState({
+    title: "",
+    description: "",
+    category: "Startup",
+    author: "Alex Bennett",
+    authorImg: "public/assets/author_img.png",
+  });
+
+  const onChangeHandler = (e: any) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+    console.log(data);
+  };
+
+  const onSubmitHandler = async (e: any) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("title", data.title);
+    formData.append("description", data.description);
+    formData.append("category", data.category);
+    formData.append("author", data.author);
+    formData.append("authorImg", data.authorImg);
+    formData.append("image", image as any);
+    const response = await axios.post("/api/blog", formData);
+    if (response.data.success){
+      alert("Blog added successfully");
+    }
+  };
 
   const handleChange = (e: any) => {
     if (e.target.files && e.target.files[0]) {
@@ -15,7 +47,7 @@ const page = () => {
 
   return (
     <div>
-      <form className="pt-5 px-5 sm:pt-12 sm:pl-16">
+      <form onSubmit={onSubmitHandler} className="pt-5 px-5 sm:pt-12 sm:pl-16">
         <p className="text-xl">Upload thumbnail</p>
         <label htmlFor="image">
           <Image
@@ -31,11 +63,39 @@ const page = () => {
         <input onChange={handleChange} type="file" id="image" hidden required />
         <p className="text-xl mt-4">Blog title</p>
         <input
+          name="title"
+          onChange={onChangeHandler}
+          value={data.title}
           className="w-full sm:w-[500px] mt-4 px-4 py-3 border"
           type="text"
           placeholder="Type here"
           required
         />
+        <p className="text-xl mt-4">Blog description</p>
+        <textarea
+          name="description"
+          onChange={onChangeHandler}
+          value={data.description}
+          className="w-full sm:w-[500px] mt-4 px-4 py-3 border"
+          placeholder="write content here"
+          rows={6}
+          required
+        />
+        <p className="text-xl mt-4">Blog category</p>
+        <select
+          name="category"
+          onChange={onChangeHandler}
+          value={data.category}
+          className="w-40 mt-4 px-4 py-3 border text-gray-500"
+        >
+          <option value="Startup">Startup</option>
+          <option value="Technology">Technology</option>
+          <option value="Lifestyle">Lifestyle</option>
+        </select>
+        <br />
+        <button type="submit" className="mt-6 w-40 h-12 bg-black text-white">
+          Add
+        </button>
       </form>
     </div>
   );
