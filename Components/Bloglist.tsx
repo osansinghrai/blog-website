ded4b366930fb1ca75d307223839137a643eb2ec
Osan Rai <osanbantawarai@gmail.com>
2025-04-12 18:36:38 +0545

    refactor(Bloglist): remove unused import of blog_data
    The `blog_data` import was unused in the Bloglist component, so it was removed to clean up the code and improve maintainability.

--- Components/Bloglist.tsx
+++ Components/Bloglist.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import Blogitem from "./Blogitem";
 import { useState } from "react";
 import axios from "axios";
-import { blog_data } from "@/public/assets/assets";
 
 const Bloglist = () => {
   const [menu, setMenu] = useState("All");
